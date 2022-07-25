3df36fb9d45bae6e88b915dfc5c1e703822f5efb
Christian Aidelsburger <c.aidelsburger@gmx.de>
2022-07-25 16:00:20 +0200

    adding function for changing status of membership

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialog, } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AddCustomerDialogComponent } from '../add-customer-dialog/add-customer-dialog.component';
 import { CustomerCardDialogComponent } from '../customer-card-dialog/customer-card-dialog.component';
 
@@ -12,10 +13,12 @@ import { CustomerCardDialogComponent } from '../customer-card-dialog/customer-ca
 
 export class CustomerComponent implements OnInit {
 
-  public customers = [];
-  firma: { name: any; company: any; membernumber: any; tel: any; mobile: any; email: any; street: any; postcode: any; town: any; entryDate: any; selectedBranch: any; membershipFee: any; };
+  customersID;
 
-  constructor(public dialog: MatDialog, public firestore: AngularFirestore) { }
+  public customers = [];
+  firma: { name: any; company: any; membernumber: any; tel: any; mobile: any; email: any; street: any; postcode: any; town: any; entryDate: any; selectedBranch: any; membershipFee: any; terminationDate: any; terminationReason: any;};
+
+  constructor(private router: Router, public dialog: MatDialog, public firestore: AngularFirestore, private route: ActivatedRoute) { }
 
   openDialog(): void {
 
@@ -25,6 +28,8 @@ export class CustomerComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(({
       name, company, membernumber, tel, mobile, email, street, postcode, town, entryDate, selectedBranch, membershipFee }) => {
+
+      let EUdate = entryDate.getDate() + "." + (entryDate.getMonth() + 1) + "." + entryDate.getFullYear()
 
       this.firma = {
         name: name,
@@ -36,24 +41,35 @@ export class CustomerComponent implements OnInit {
         street: street,
         postcode: postcode,
         town: town,
-        entryDate: entryDate.toISOString().split('T')[0],
+        entryDate: EUdate,
         selectedBranch: selectedBranch,
         membershipFee: membershipFee,
+        terminationDate: '',
+        terminationReason: '',
       };
 
       this.firestore
         .collection('Kunden')
         .add(this.firma)
+        .then( (customerInfo:any) => {
+
+          this.firestore
+          .collection('Kunden')
+          .doc(customerInfo.id)
+          .update({ CustomersID: customerInfo.id });
+        });
+       
     });
   }
 
   openDialog2(i) {
     const dialogRef = this.dialog.open(CustomerCardDialogComponent);
     dialogRef.componentInstance.i = i;
-    console.log(i)
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
+    // console.log(this.customersID)
+    // this.router.navigateByUrl('/' + this.customersID)
   }
 
   ngOnInit(): void {
@@ -63,5 +79,6 @@ export class CustomerComponent implements OnInit {
       .subscribe((customer: any) => {
         this.customers = customer;
       });
+
   }
 }
--- src/app/set-termination/set-termination.component.ts
+++ src/app/set-termination/set-termination.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-set-termination',
@@ -9,9 +11,39 @@ export class SetTerminationComponent implements OnInit {
 
   terminationDate: any;
   terminationReason: any;
-  constructor() { }
+
+  myID: any;
+
+  constructor(private route: ActivatedRoute, public firestore: AngularFirestore) { }
 
   ngOnInit(): void {
   }
 
+  saveTermination() {
+    let dateOfTermination = this.terminationDate.getDate() + "." + (this.terminationDate.getMonth() + 1) + "." + this.terminationDate.getFullYear();
+    console.log(dateOfTermination)
+
+    // this.route.params.subscribe((params) => {
+    //   console.log(params);
+
+    // });
+    this.firestore
+      .collection('Kunden', ref => ref.where('name', '==', 'a'))
+      .valueChanges()
+      .subscribe((a: any) => {
+        this.myID = a[0].CustomersID;
+
+        this.firestore
+      .collection('Kunden')
+      .doc(a[0].CustomersID)
+      .update({status: 'gekündigt'})
+
+      })
+
+      
+
+
+    // .doc('B8LCDqi0ZO3i5trNbAey')
+    //   .update({ terminationDate: dateOfTermination, terminationReason: this.terminationReason });
+  }
 }
