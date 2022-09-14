e650d712c7e49922e596b5094b6c20f4954d4876
Christian Aidelsburger <c.aidelsburger@gmx.de>
2022-09-14 21:31:32 +0200

    implemented customer details for dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSliderModule } from '@angular/material/slider';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { CustomerComponent } from './customer-section/customer/customer.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { AddCustomerDialogComponent } from './customer-section/add-customer-dialog/add-customer-dialog.component';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore/'; 
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore/';
 import { AngularFireModule } from '@angular/fire/compat';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { SetTerminationComponent } from './customer-section/set-termination/set-termination.component';
 import { CustomersService } from './customer-section/customers.service';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { DeletingCustomerDialogComponent } from './customer-section/deleting-customer-dialog/deleting-customer-dialog.component';
 import { EditCustomerComponent } from './customer-section/edit-customer/edit-customer.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { CustomerDetailsComponent } from './customer-section/customer-details/customer-details.component';
 import { HelpSectionComponent } from './help-section/help-section.component';
 import { TasksOverviewComponent } from './task-section/tasks-overview/tasks-overview.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -82,8 +83,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     MatCheckboxModule,
     MatToolbarModule,
     MatTooltipModule,
+    MatSlideToggleModule,
     AngularFireModule.initializeApp(environment.firebase), // compat API
-    
+
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,10 +9,27 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(public firestore: AngularFirestore) { }
+  constructor(public firestore: AngularFirestore, private _formBuilder: FormBuilder) { }
+
+  isChecked = false;
+  formGroup = this._formBuilder.group({
+    enableWifi: '',
+    acceptTerms: ['', Validators.requiredTrue],
+  });
+
+  onFormSubmit() {
+    alert(JSON.stringify(this.formGroup.value, null, 2));
+  }
 
   fees = [];
   sum = 0;
+  show: boolean = false;
+  amountOfCustomers: number = 0;
+  amountOfTerminatedCustomers: number = 0;
+
+
+
+  terminatedCustomer = [];
 
   ngOnInit(): void {
     this.getDataFromFirebase();
@@ -23,19 +41,36 @@ export class DashboardComponent implements OnInit {
       .valueChanges()
       .subscribe((customer: any) => {
         this.getMembershipFees(customer);
-        // console.log(customer)
+        this.getTerminatedCustomer(customer);
       });
   }
 
   getMembershipFees(customer) {
-    for(let i = 0; i < customer.length; i++ ) {
-      if(customer[i].status == 'aktiv' && customer[i].member == 'member')
-      this.fees.push(customer[i].membershipFee);
-      console.log(customer[i].membershipFee)
-      // this.sum += 3;
-    }
+    for (let i = 0; i < customer.length; i++) {
+      if (customer[i].status == 'aktiv' && customer[i].member == 'member') {
+        this.fees.push(customer[i].membershipFee);
+        this.sum += +customer[i].membershipFee;
 
-    // console.log(this.sum)
-    
+        // findes out how many custermers the company has
+        this.amountOfCustomers++;
+      }
+    }
   }
+
+  getTerminatedCustomer(customer) {
+    for (let i = 0; i < customer.length; i++) {
+      if (customer[i].status == 'gekündigt') {
+
+        this.terminatedCustomer.push(
+          {
+            name: customer[i].company,
+            reason: customer[i].terminationReason,
+            date: customer[i].terminationDate
+          }
+        );
+        this.amountOfTerminatedCustomers++;
+      }
+    }
+  }
+
 }
